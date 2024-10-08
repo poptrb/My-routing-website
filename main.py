3b6e6a7605632180fdce2b4f76a0e1c3333f3395
Iulian Popescu <popescuiulian67@gmail.com>
2024-10-08 13:06:25 +0300

    map dragging done

--- main.py
+++ main.py
@@ -3,19 +3,20 @@
 from time import sleep
 
 from seleniumwire import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options, DesiredCapabilities
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.actions.mouse_button import MouseButton
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 from utils.utils import (
     cull_request_list,
     format_response,
     process_geojson_response,
-    pan_map
+    pan_map,
 )
 
 
@@ -27,40 +28,51 @@ def setup_driver():
     options = Options()
     # options.add_argument('--headless')
     # options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()), options=options)
+    options.add_argument("--disable-dev-shm-usage")
+    # driver = webdriver.Chrome(
+    #         service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
     driver.maximize_window()
     driver.get(
-        "https://www.waze.com/live-map?" +
-        "utm_source=waze_website&utm_campaign=waze_website%27"
+        "https://www.waze.com/live-map?"
+        + "utm_source=waze_website&utm_campaign=waze_website%27"
     )
 
     try:
 
         def acknowledge_banners(
-          driver,
-          html_classes=['wz-cc-disallow', 'waze-tour-tooltip__acknowledge'],
-          css_selectors=['.wz-downloadbar__close-button']):
+            driver,
+            html_classes=["wz-cc-disallow", "waze-tour-tooltip__acknowledge"],
+            css_selectors=[".wz-downloadbar__close-button"],
+        ):
             """
             Take care of consent banner, and other visual annoyances
-             """
+            """
 
             for _ in html_classes:
                 element = WebDriverWait(driver, 7).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, _)))
+                    EC.presence_of_element_located((By.CLASS_NAME, _))
+                )
                 print(f"Found HTML element with class: {_}")
                 element.click()
 
             for _ in css_selectors:
                 element = WebDriverWait(driver, 7).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, _)))
+                    EC.presence_of_element_located((By.CSS_SELECTOR, _))
+                )
                 print(f"Found HTML element with CSS selector: {_}")
                 element.click()
 
         acknowledge_banners(driver)
-        print('Initalized web driver!')
+
+        plus_icon = driver.find_element(
+            By.XPATH, './/i[contains(@class, "w-icon-plus")]'
+        )
+        plus_icon.click()
+        sleep(2)
+
+        print("Initalized web driver!")
 
         return driver
     except Exception as e:
@@ -70,14 +82,18 @@ def setup_driver():
 
 def process_georss_requests(driver):
     georss_requests = [
-            x for x in driver.requests if 'live-map/api/georss' in x.url]
+        x for x in driver.requests if "live-map/api/georss" in x.url
+    ]
 
-    print(f"Total requests: {len(driver.requests)}",
-          f"GeoRSS requests: {len(georss_requests)}.")
+    print(
+        f"Total requests: {len(driver.requests)}",
+        f"GeoRSS requests: {len(georss_requests)}.",
+    )
 
     for request in georss_requests:
-        if 'live-map/api/georss' in request.url and hasattr(
-                request.response, 'body'):
+        if "live-map/api/georss" in request.url and hasattr(
+            request.response, "body"
+        ):
 
             response = format_response(request.response)
 
@@ -91,8 +107,18 @@ def main_loop(driver):
 
         process_georss_requests(driver)
         # cull_request_list(driver)
-        pan_map(driver)
-        sleep(10)
+        # breakpoint()
+        clientHeight = driver.execute_script(
+            "return document.documentElement.clientHeight"
+        )
+        clientWidth = driver.execute_script(
+            "return document.documentElement.clientWidth"
+        )
+        pan_map(
+            driver,
+            viewport_size={"width": clientWidth, "height": clientHeight},
+        )
+        sleep(4.5)
 
 
 def main():
@@ -100,5 +126,5 @@ def main():
     main_loop(driver)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- utils/utils.py
+++ utils/utils.py
@@ -1,27 +1,34 @@
-import math
-import json
 from datetime import datetime, timedelta
+import json
+import math
+import time
+import pdb
 
-from selenium.webdriver import ActionChains
+from selenium.webdriver import ActionChains, Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import (
+    StaleElementReferenceException,
+    MoveTargetOutOfBoundsException,
+)
 
 
 def format_record(record):
-    record['pubTime'] = datetime.utcfromtimestamp(record['pubMillis']/1000.0)
-    record['pubTime'] = record['pubTime'].strftime('%Y-%m-%d %H:%M:%S')
+    record["pubTime"] = datetime.utcfromtimestamp(
+        record["pubMillis"] / 1000.0
+    ).strftime("%Y-%m-%d %H:%M:%S")
 
     return record
 
 
 def print_record(record):
     _ = {
-        'wazeData': record['wazeData'],
-        'pubTime': record['pubTime'],
-        'street': record['street'] if 'street' in record.keys() else '',
-        'uuid': record['uuid'],
-        'location': record['location']
+        "wazeData": record["wazeData"],
+        "pubTime": record["pubTime"],
+        "street": record["street"] if "street" in record.keys() else "",
+        "uuid": record["uuid"],
+        "location": record["location"],
     }
 
     print(json.dumps(record, indent=4))
@@ -35,101 +42,98 @@ def format_response(response):
             "reason": response.reason,
             "headers": dict(response.headers),
             "date": response.date.strftime("%Y-%m-%d %H:%M:%S"),
-            "body": response.body.decode('utf-8')
+            "body": response.body.decode("utf-8"),
         }
         return formatted_response
     except Exception as e:
-        print(f"RESPONSE STRUCTURE EXCEPTION: {response.__class__}: ",
-              f"{str(dir(response))}",
-              f"Original exception: {e}")
+        print(
+            f"RESPONSE STRUCTURE EXCEPTION: {response.__class__}: ",
+            f"{str(dir(response))}",
+            f"Original exception: {e}",
+        )
     else:
         return None
 
 
 def process_geojson_response(response):
-    data = json.loads(response['body'])
-    for record in filter(lambda x: x['type'] in ['POLICE'], data['alerts']):
-        record = format_record(record)
+    if response["body"]:
+        data = json.loads(response["body"])
+        for record in filter(
+            lambda x: x["type"] in ["POLICE"], data["alerts"]
+        ):
+            record = format_record(record)
 
-        print_record(record)
+        # print_record(record)
 
 
 def cull_request_list(driver):
     current_time = datetime.now()
     one_minute_ago = current_time - timedelta(minutes=1)
-    filtered_list = [entry for entry in driver.requests if datetime.fromtimestamp(entry['pubMillis']) < one_minute_ago]
+    filtered_list = [
+        entry
+        for entry in driver.requests
+        if datetime.fromtimestamp(entry["pubMillis"]) < one_minute_ago
+    ]
     print(
-      len(filtered_list),
-      [format_response(req)['date'] for req in driver.requests])
+        len(filtered_list),
+        [format_response(req)["date"] for req in driver.requests],
+    )
 
 
-def pan_map(driver):
-    _ = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located(
-            (By.XPATH, './/img[contains(@class, "leaflet-tile-loaded")]')))
-
-    leaflet_tiles = driver.find_elements(
-        By.XPATH, './/img[contains(@class, "leaflet-tile-loaded")]')
-
-    print(f"Found {len(leaflet_tiles)} Leaflet tiles!")
-
-    tiles_pos = [
-        (tile.location['x'], tile.location['y']) for tile in leaflet_tiles ]
-
-    c_x, c_y = find_central_point(tiles_pos)
-    t_x, t_y = find_bounding_points(tiles_pos)[1]
-
-    center_tile = list(filter(
-        lambda x: x.location['x'] == c_x and x.location['y'] == c_y,
-        leaflet_tiles))[0]
-
-    target_tile = list(filter(
-        lambda x: x.location['x'] == t_x and x.location['y'] == t_y,
-        leaflet_tiles))[0]
-
-    ActionChains(driver).\
-        drag_and_drop_by_offset(
-            center_tile,
-            (target_tile.location['x'] - center_tile.location['x']) / 2,
-            (target_tile.location['y'] - center_tile.location['y']) / 2).\
-        perform()
-#
-    print("Moved mouse!")
+def find_tile_by_loc(tiles, p):
+    try:
+        return list(
+            filter(
+                lambda _: _.location["x"] == p[0] and _.location["y"] == p[1],
+                tiles,
+            )
+        )[0]
+    except Exception as e:
+        print(
+            f"Could not find tile with location {(p[0], p[1])} in tile list!",
+            e,
+        )
+        return None
 
 
-def find_bounding_points(points):
-    farthest_up = max(points, key=lambda point: point[1])
-    farthest_down = min(points, key=lambda point: point[1])
-    farthest_east = max(points, key=lambda point: point[0])
-    farthest_west = min(points, key=lambda point: point[0])
-
-    print( f"Farthest points: {[farthest_down, farthest_up, farthest_west, farthest_east]}")
-    return [farthest_down, farthest_up, farthest_west, farthest_east]
+def get_viewport_latlng(driver: Firefox) -> (int, int):
+    el = driver.find_element(By.CLASS_NAME, "wm-attribution-control__latlng")
+    el_latlng = el.find_element(By.XPATH, "./child::*")
+    lat, lng = [_.strip() for _ in el_latlng.text.split("|")]
+    return (lat, lng)
 
 
-def find_central_point(points):
-    center_x = sum(x for x, y in points) / len(points)
-    center_y = sum(y for x, y in points) / len(points)
+def pan_map(driver: Firefox, viewport_size: dict = None):
 
-    def distance(point):
-        x, y = point
-        return math.sqrt((x - center_x) ** 2 + (y - center_y) ** 2)
+    map_container = driver.find_element(
+        By.XPATH, './/div[contains(@class,"wm-map__leaflet wm-map")]'
+    )
 
-    result = min(points, key=distance)
-    print(f"Center point of tiles is {result}")
-    return result
+    try:
+        # map_container, window_size['width'] / 2, 0  # pan to left
+        # map_container, -1 * window_size['width'] / 2, 0  # pan to right
+        # map_container, 0, -1 * window_size['height'] / 2.0  # pan upwards
+        # map_container, 0, window_size['height'] / 2.0  # pan downwards
 
-# action.pointer_action.move_to_location(8, 0)
-# draggable = driver.find_element(By.ID, "draggable")
-# map = driver.find_element(By.CLASS_NAME, "wm-map__leaflet wm-map leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom wm-map--zoom-medium wm-map--zoom-14")
-#
-# map_location = map.location
-#
-# finish = driver.find_element(By.ID, "droppable").location
-#
-# action = ActionBuilder(driver)
-# action.pointer_action.=
-# ActionChains(driver).drag_and_drop_by_offset(
-#     draggable,
-#     finish['x'] - start['x'], finish['y'] - start['y']
-# ).perform()
+        # pdb.set_trace()
+        for _ in range(3):
+            ActionChains(driver).drag_and_drop_by_offset(
+                map_container, 0, (viewport_size["height"] // 2)
+            ).perform()
+            print(get_viewport_latlng(driver))
+            time.sleep(1)
+
+        for _ in range(3):
+            ActionChains(driver).drag_and_drop_by_offset(
+                map_container, viewport_size["width"] // 2, 0
+            ).perform()
+            time.sleep(1)
+
+    except MoveTargetOutOfBoundsException as e:
+        print(
+            "Could not move mouse from location: ",
+            f"{map_container.location['x']},{map_container.location['y']}\n",
+            e,
+            sep=" ",
+        )
+        print("Moved mouse!")
