add5ac586bbbffa37e448e3e15968411457256b4
Iulian Popescu <popescuiulian67@gmail.com>
2024-10-16 15:51:36 +0300

    Postgres connection string and JWT params

---
+++ containers/backend/src/settings.py
@@ -0,0 +1,19 @@
+import os
+
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    asyncpg_url: PostgresDsn = (
+        "postgresql+asyncpg://"
+        + f"{os.getenv("POSTGIS_USER")}:{os.getenv("POSTGIS_PASSWORD")}@"
+        + f"{os.getenv("POSTGIS_HOST")}:{os.getenv("POSTGIS_PORT")}/"
+        + f"{os.getenv("POSTGIS_DB")}"
+    )
+    jwt_algorithm: str = os.getenv("JWT_ALGORITHM")
+    jwt_expire: int = os.getenv("JWT_EXPIRE")
+    jwt_secret: str = os.getenv("JWT_SECRET")
+
+
+settings = Settings()
