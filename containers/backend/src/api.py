2d8c5925d28a5cdd06697c41f4dff0aae8735948
Iulian Popescu <popescuiulian67@gmail.com>
2024-10-16 15:50:46 +0300

    feat: use apscheduler to orchestrare long-running tasks

--- containers/backend/src/api.py
+++ containers/backend/src/api.py
@@ -1,24 +1,50 @@
+import asyncio
 import json
 import os
+import time
+import logging
+from datetime import datetime
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
+from tasks.geo_rss import refresh_reports
+from database import create_db_and_tables
+
+
+logging.basicConfig(
+    format='%(levelname)s %(asctime)s %(module)s %(message)s',
+    datefmt='%Y/%m/%d %H:%M:%S',
+    level=logging.DEBUG
+)
+
+def run_scheduler():
+    logging.getLogger("apscheduler").setLevel(logging.DEBUG)
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(refresh_reports, "interval", seconds=900,next_run_time=datetime.now())
+
+    scheduler.start()
+
+
 app = FastAPI()
-
-
-origins = [
-    "http://localhost",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:8000", "localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.on_event("startup")
+async def on_startup():
+    logging.getLogger("database").setLevel(logging.DEBUG)
+    logging.getLogger("tasks.geo_rss").setLevel(logging.DEBUG)
+    await create_db_and_tables()
+    run_scheduler()
 
 
 @app.get("/")
@@ -28,6 +54,7 @@ def read_json_file():
     return data
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT', 8001)))
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("API_PORT", 80)))
