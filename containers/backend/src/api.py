48bd6579c374b7c7b27b7c738c464ecf24b9cf5e
Iulian Popescu <popescuiulian67@gmail.com>
2024-11-06 16:39:44 +0200

    feat: authentication with fastapi-users

--- containers/backend/src/api.py
+++ containers/backend/src/api.py
@@ -4,17 +4,32 @@ import os
 import time
 import logging
 from datetime import datetime, timedelta
+from typing import List
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 
 from tasks.geo_rss import refresh_reports
 from database import create_db_and_tables, SessionDep
-from database.operations import get_reports, get_reports_two
-from database.schemas import ReportBase
-from database.schema import GetReportsRequest
+from database.operations import (
+    get_reports,
+    get_reports_two,
+    insert_token,
+    get_token,
+)
+from database.schemas import (
+    SignupTokenModel,
+    SignupTokenModelRead,
+    ReportBase,
+    UserRead,
+    UserCreate,
+    UserUpdate,
+    GetReportsRequest,
+)
+
+from database.models import User, SignupToken
 from auth.users import auth_backend, current_active_user, fastapi_users
 
 
@@ -63,6 +78,36 @@ app.include_router(
     tags=["auth"],
 )
 
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
+
 
 @app.get("/", response_model=list[ReportBase])
 async def get_top_reports(db_session: SessionDep):
@@ -75,6 +120,14 @@ async def get_reports_by_bbox(
     request: GetReportsRequest, db_session: SessionDep
 ):
     result = await get_reports_two(db_session, request)
+    return result
+
+
+@app.post("/create_token", response_model=SignupTokenModelRead)
+async def create_token(request: SignupTokenModel, db_session: SessionDep):
+    await insert_token(db_session, request)
+
+    result = await get_token(db_session, request.token_hash)
     return result
 
 
--- containers/backend/src/auth/users.py
+++ containers/backend/src/auth/users.py
@@ -1,22 +1,33 @@
 import uuid
 from typing import Optional
+from logging import getLogger
 
-from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin, models
+from fastapi import Depends, Request, HTTPException
+from fastapi_users import (
+    BaseUserManager,
+    FastAPIUsers,
+    UUIDIDMixin,
+    models,
+)
+
 from fastapi_users.authentication import (
     AuthenticationBackend,
     BearerTransport,
     JWTStrategy,
 )
 from fastapi_users.db import SQLAlchemyUserDatabase
-from fastapi_users import schemas
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.operations import get_user_db
+from database import SessionDep, get_async_session, async_session_maker
 from database.models import User
-from database.operations import get_token, insert_token
+from database.schemas import UserRead, UserCreate
+from database.operations import get_token, insert_token, invalidate_token
 from settings import settings
 
 SECRET = "SECRET"
+
+
+logger = getLogger()
 
 
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
@@ -25,16 +36,31 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
 
     async def create(
         self,
-        user_create: schemas.UC,
+        user_create: UserCreate,
         safe: bool = False,
         request: Optional[Request] = None,
-    ) -> models.UP:
-        super().__init__()
+    ) -> User:
+
+        async with async_session_maker() as db_session:
+            token = await get_token(db_session, user_create.token_cleartext)
+
+            if not token:
+                raise HTTPException(401, detail="Invalid token!")
+
+            user_create.token_id = token.id
+            del (user_create.token_cleartext)
+
+        created_user = await super().create(user_create, safe, request=request)
+        return created_user
 
     async def on_after_register(
         self, user: User, request: Optional[Request] = None
     ):
-        print(f"User {user.id} has registered.")
+
+        async with async_session_maker() as db_session:
+            await invalidate_token(db_session, User.token_id)
+
+        logger.info(f"User {user.id} has registered.")
 
     async def on_after_forgot_password(
         self, user: User, token: str, request: Optional[Request] = None
@@ -49,6 +75,10 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
         print(
             f"Verification requested for user {user.id}. Verification token: {token}"
         )
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
 
 
 async def get_user_manager(
@@ -69,6 +99,7 @@ auth_backend = AuthenticationBackend(
     transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
+
 
 fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
 
