519e6a99cb943dcc91af4967f708c4bff72baeef
Iulian Popescu <popescuiulian67@gmail.com>
2024-11-11 23:26:42 +0200

    add react-router to handle login, register, etc

---
+++ containers/react-frontend/src/Router.js
@@ -0,0 +1,34 @@
+import {Route, Routes} from 'react-router-dom';
+
+import PrivateRoutes from './PrivateRoutes';
+import {CustomMap} from './Map.js'
+import {ExternalProvider} from './ReportsProvider'
+import {Login} from './screens/Login';
+import {Register} from './screens/Register';
+
+export function Router() {
+  return (
+    <Routes>
+      <Route
+        path='/register'
+        element={<Register />}
+      />
+      <Route
+        path='/login'
+        element={<Login />}
+      />
+      <Route
+        element={<PrivateRoutes />}
+      >
+        <Route
+          path={"/explore"}
+          element={<CustomMap />}
+        />
+      </Route>
+    </Routes>
+  );
+}
+      // <Route element={<PrivateRoutes />}>
+      //   <Route path='/logged' element={<Logged />} />
+      // </Route>
+      // <Route path='/' element={<LoginPage />} exact />
