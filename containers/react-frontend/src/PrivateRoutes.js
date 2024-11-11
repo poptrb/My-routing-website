f2eba75d94f8c0a4bb58172a24ea10824023c3f2
Iulian Popescu <popescuiulian67@gmail.com>
2024-11-11 23:29:06 +0200

    add private route handler

---
+++ containers/react-frontend/src/PrivateRoutes.js
@@ -0,0 +1,21 @@
+import useAuth from './context/AuthProvider.js'
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
+
+const PrivateRoutes = () => {
+  const { auth } = useAuth();
+  const location = useLocation();
+
+  // const jwt_token = localStorage.getItem('jwt_token');
+
+  return (
+    auth?.user
+      ? <Outlet />
+      : <Navigate
+          to='/login'
+          state={{from: location}}
+          replace
+        />
+  );
+};
+
+export default PrivateRoutes;
