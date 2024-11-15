b1d71a47363c6dfce4561b616f0d0bc2001b4220
Iulian Popescu <popescuiulian67@gmail.com>
2024-11-15 14:40:40 +0200

    add react-query

--- containers/react-frontend/src/App.js
+++ containers/react-frontend/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from 'react-query'
 
 import {Router} from './Router';
 import {AuthProvider} from './context/AuthProvider'
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Router/>
+        <QueryClientProvider client={queryClient}>
+          <Router/>:
+        </QueryClientProvider>
       </AuthProvider>
     </BrowserRouter>
   )
