7f76ab8518ee9def310d50011930ce468f12ab29
Iulian Popescu <popescuiulian67@gmail.com>
2025-04-02 21:06:52 +0300

    change font

--- containers/react-frontend/src/index.js
+++ containers/react-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+import './fonts/fonts.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
